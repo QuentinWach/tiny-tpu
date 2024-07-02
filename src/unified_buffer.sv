d5f0788bf06c4033f05832385a3bc158b86f4c52
Evan Lin <62809897+eevaain@users.noreply.github.com>
2024-07-02 15:01:31 -0700

    moved integer i outside the always block

--- src/unified_buffer.sv
+++ src/unified_buffer.sv
@@ -14,14 +14,10 @@ module unified_buffer (
   input [31:0] acc2_mem_0,
   input [31:0] acc2_mem_1,
 
-  // output reg [31:0] unified_mem [0:63],
-
-  // triggered on write operation 
   output reg [31:0] out_ub_00,
   output reg [31:0] out_ub_01,
   output reg [31:0] out_ub_10,
   output reg [31:0] out_ub_11
-  // make another output (4 outputs registers) to output just the input activations to the input setup module
 );
 
   // the local memory
@@ -30,9 +26,12 @@ module unified_buffer (
   // Internal counter to keep track of the next free memory location
   reg [5:0] write_pointer;
 
+  // Initialize local variables
+  integer i;
+
   always @(posedge clk or posedge reset) begin
     if (reset) begin
-      integer i;
+      // Initialize memory and outputs on reset
       for (i = 0; i < 64; i = i + 1) begin
         unified_mem[i] <= 0;
       end
@@ -50,10 +49,7 @@ module unified_buffer (
       unified_mem[16'h0020] <= 21;
       unified_mem[16'h0021] <= 22;
     end else begin
-
-      /* WRITE TO MEMORY
-          Handle data coming from accumulators that is going into unified buffer
-      */
+      // Handle data coming from accumulators that is going into unified buffer
       if (store && store_acc1 && store_acc2) begin
         unified_mem[addr] <= acc1_mem_0;
         unified_mem[addr + 1] <= acc1_mem_1;
@@ -62,9 +58,8 @@ module unified_buffer (
         write_pointer <= addr + 4;
       end
       
-      /* READ FROM MEMORY
-      */  
-      if (load_input) begin // if load_input flag is on, then load data from unified buffer to input setup buffer
+      // Handle loading data from unified buffer to input setup buffer
+      if (load_input) begin
         out_ub_00 <= unified_mem[addr]; 
         out_ub_01 <= unified_mem[addr + 1]; 
         out_ub_10 <= unified_mem[addr + 2]; 
@@ -72,10 +67,5 @@ module unified_buffer (
       end
     end
   end
-
-  // Print the accumulator values every clock cycle
-  // always @(posedge clk) begin
-  //   $display("Time: %0t | acc1_mem_0: %0d, acc1_mem_1: %0d, acc2_mem_0: %0d, acc2_mem_1: %0d", $time, acc1_mem_0, acc1_mem_1, acc2_mem_0, acc2_mem_1);
-  // end
 
 endmodule
