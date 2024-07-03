4787b9bf44215282d527363a7b4162bc56b93d52
Evan Lin <62809897+eevaain@users.noreply.github.com>
2024-07-02 17:49:01 -0700

    conditons have no overlap now idk?

--- src/unified_buffer.sv
+++ src/unified_buffer.sv
@@ -38,27 +38,19 @@ module unified_buffer (
       out_ub_10 <= 0;
       out_ub_11 <= 0; 
 
-      // Dummy activation values
-      // unified_mem[16'h001E] <= 11;
-      // unified_mem[16'h001F] <= 12;
-      // unified_mem[16'h0020] <= 21;
-      // unified_mem[16'h0021] <= 22;
-    end else begin
+    end else if (store && store_acc1 && store_acc2) begin
       // Handle data coming from accumulators that is going into unified buffer
-      if (store && store_acc1 && store_acc2) begin
-        unified_mem[addr] <= acc1_mem_0;
-        unified_mem[addr + 1] <= acc1_mem_1;
-        unified_mem[addr + 2] <= acc2_mem_0;
-        unified_mem[addr + 3] <= acc2_mem_1;
-      end
-      
+      unified_mem[addr] <= acc1_mem_0;
+      unified_mem[addr + 1] <= acc1_mem_1;
+      unified_mem[addr + 2] <= acc2_mem_0;
+      unified_mem[addr + 3] <= acc2_mem_1;
+
+    end else if (load_input) begin
       // Handle loading data from unified buffer to input setup buffer
-      if (load_input) begin
-        out_ub_00 <= unified_mem[addr]; 
-        out_ub_01 <= unified_mem[addr + 1]; 
-        out_ub_10 <= unified_mem[addr + 2]; 
-        out_ub_11 <= unified_mem[addr + 3]; 
-      end
+      out_ub_00 <= unified_mem[addr]; 
+      out_ub_01 <= unified_mem[addr + 1]; 
+      out_ub_10 <= unified_mem[addr + 2]; 
+      out_ub_11 <= unified_mem[addr + 3]; 
     end
   end
 
